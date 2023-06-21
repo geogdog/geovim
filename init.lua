55a17a5267faf12012368b6b26bc1460887616bb
Greg Trahair <greg.trahair@gmail.com>
2023-06-21 14:14:02 +0200

    Add personal Snippets

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("luasnip/loaders/from_vscode").load({
+  paths = {
+    "~/.config/nvim/geo-snippets",
+  },
+})
+
+require("luasnip").filetype_extend("python", { "django" })
